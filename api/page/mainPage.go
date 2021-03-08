97f5e1fe4c1d8668cd1eaeebf60055e1fd375dcd
Sergei Shishkin <3shishkin39@mail.ru>
2021-03-09 00:19:17 +0300

    получение рестаранов по контексту

--- api/page/mainPage.go
+++ api/page/mainPage.go
@@ -7,27 +7,6 @@ import (
 	"strconv"
 )
 
-type restaurantResponse struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	DeliveryCost int    `json:"deliveryCost"`
-}
-
-func initRestaurants() []api.Restaurant {
-	restaurants := make([]api.Restaurant, 0, 10)
-
-	for i := 0; i < 10; i++ {
-		res := api.Restaurant{}
-		res.DeliveryCost = 10
-		res.Name = "My rest"
-		res.ID = i
-
-		restaurants = append(restaurants, res)
-	}
-
-	return restaurants
-}
-
 // загрузка списка рестаранов
 func GetVendor(c echo.Context) error {
 	Limit, errLimit := strconv.Atoi(c.QueryParam("limit"))
@@ -41,23 +20,19 @@ func GetVendor(c echo.Context) error {
 		})
 	}
 
-	result := getRestaurant(Limit, Offset)
+	cc := c.(*api.CustomContext)
+
+	result := getRestaurant(Limit, Offset, cc)
 	return c.JSON(http.StatusOK, result)
 }
 
 // в будущем здесь будет поход в базу данных
-func getRestaurant(limit, offset int) []restaurantResponse {
-	var result []restaurantResponse
+func getRestaurant(limit, offset int, context *api.CustomContext) []api.Restaurant {
+	var result []api.Restaurant
 
-	restaurants := initRestaurants()
+	for _, restaurant := range *context.Restaurants {
+		if restaurant.ID >= offset && restaurant.ID < offset+limit {
 
-	for _, val := range restaurants {
-		if val.ID >= offset && val.ID < offset+limit {
-			restaurant := restaurantResponse{
-				ID:           val.ID,
-				Name:         val.Name,
-				DeliveryCost: val.DeliveryCost,
-			}
 			result = append(result, restaurant)
 		}
 	}
