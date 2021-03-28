5092ed7ac76dc673ccbc25d0223c5460fdffd21f
Sergei Shishkin <3shishkin39@mail.ru>
2021-03-28 15:35:24 +0300

    изменены данные бд

--- config/config.go
+++ config/config.go
@@ -1,15 +1,16 @@
 package config
 
 const (
-	Host          = "http://89.208.197.150"
+	// Host          = "http://89.208.197.150"
+	Host          = "http://127.0.0.1"
 	Repository    = Host + ":5000/"
 	Client        = Host + ":3000/"
 	DefaultAvatar = Repository + "static/avatar/stas.jpg"
 	SessionCookie = "borscht_session"
 	Static        = "static/avatar"
 	PostgresDB    = "postgres"
-	DBUser        = "labzunova" // todo ...
-	DBPass        = "1111"
-	DBName        = "postgres"
+	DBUser        = "delivery" // todo ...
+	DBPass        = "password"
+	DBName        = "delivery_psql"
 	CORStime      = 3600
 )
