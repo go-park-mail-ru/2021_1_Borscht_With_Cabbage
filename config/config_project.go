0766e9d440e4e83867ee40dde6b8110887e5232b
Sergei Shishkin <3shishkin39@mail.ru>
2021-05-04 18:33:36 +0300

    correct config

---
+++ config/config_project.go
@@ -0,0 +1,16 @@
+package config
+
+const (
+	DefaultUserImage       = Repository + "default/user/avatar/avatar.png"
+	DefaultRestaurantImage = Repository + "default/restaurant/restaurant.png"
+	DefaultDishImage       = Repository + "default/dish/dish.png"
+	SessionCookie          = "borscht_session"
+	LifetimeSecond         = 86400
+	KeyParams              = "key"
+
+	TimeFormat = "01.01.2021 21:00:04"
+
+	CORStime  = 3600
+	RoleAdmin = "admin"
+	RoleUser  = "user"
+)
