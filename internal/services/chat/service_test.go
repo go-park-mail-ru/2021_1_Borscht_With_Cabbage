c87854e31d0ebd4f1c939c95a8d361548c72f130
labzunova <dasha.labzunova@mail.ru>
2021-05-06 18:55:21 +0300

    couple of tests added

---
+++ internal/services/chat/service_test.go
@@ -0,0 +1,20 @@
+package chat
+
+import (
+	"testing"
+)
+
+func TestService_GetAllChats(t *testing.T) {
+	//ctrl := gomock.NewController(t)
+	//defer ctrl.Finish()
+	//clientMock := mocks.NewMockChatClient(ctrl)
+	//chatService := NewService(clientMock)
+}
+
+func TestService_GetAllMessages(t *testing.T) {
+
+}
+
+func TestService_ProcessMessage(t *testing.T) {
+
+}
